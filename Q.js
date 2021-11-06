2b7a767c093c3dc35c93e9b6780ec712f2294f45
rohan <r.s.sumant@gmail.com>
2021-11-06 15:29:14 -0700

    Added a Queue implementation

---
+++ Q.js
@@ -0,0 +1,38 @@
+class Q {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  get() {
+    return this.head;
+  }
+
+  append(val) {
+    let newNode = new QNode(val);
+    if(this.tail) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+    this.tail = newNode;
+  }
+
+  remove() {
+    if(this.head) {
+      this.head = this.head.next;
+      if(!this.head) {
+        this.tail = null;
+      }
+    }
+  }
+};
+
+class QNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+};
+
+module.exports = Q;
