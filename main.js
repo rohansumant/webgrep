21c555f29319d8253708dfa06c62b543f99e1801
rohan <r.s.sumant@gmail.com>
2021-11-11 19:09:09 -0800

    Work in progress

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const $ = require('jquery');
 const {Queue} = require('@datastructures-js/queue');
 
+var globalStop = false;
+
 function webgrep(pattern) {
+  globalStop = false;
+  window.addEventListener('mousedown',(e) => {
+    globalStop = true;
+  });
+
+
+  let visited = new Set();
+  const urlQueue = Queue.fromArray([window.location]);
+  while(!urlQueue.isEmpty() && !globalStop) {
+    const currLink = urlQueue.front().slice();
+    urlQueue.dequeue();
+    
+  }
 
 }
+
