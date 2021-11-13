60f3b3df829c068e538f87ac4c5e2c6559b0186a
rohan <r.s.sumant@gmail.com>
2021-11-13 13:35:56 -0800

    First working implementation

---
+++ buildUserScript.js
@@ -0,0 +1,31 @@
+fs = require('fs')
+
+const prefix = `// ==UserScript==
+// @name         Grep
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Rohan Sumant
+// @match        https://*/*
+
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+`;
+ 
+const suffix = `(function() {
+    'use strict';
+    window.webgrep = webgrep;
+    // Your code here...
+})();
+`; 
+var content = '';
+try {
+  let data = fs.readFileSync('bundle.js', 'utf8');
+  content = prefix + data + suffix;
+  fs.writeFileSync('grep.user.js',content);
+} catch(err) {
+  console.log('Error building user script',err);
+}
+
--- main.js
+++ main.js
@@ -1,22 +1,74 @@
-const $ = require('jquery');
 const {Queue} = require('@datastructures-js/queue');
+const axios = require('axios');
 
 var globalStop = false;
 
-function webgrep(pattern) {
+function processPage(link, page,
+  queue, visited, pattern) {
+  
+  // Print out all matches.
+
+  let el = document.createElement('html');
+  el.innerHTML = page;
+  let text = el.textContent;
+  let matches = [...text.matchAll(new RegExp(pattern,'g'))];
+
+  if(matches.length > 0) {
+    console.log('Match: ',link);
+  }
+
+
+  // Extract links on the page and add them to the queue
+
+  let linksRE = /href="(https:.*?)"/g;
+  let newLinks = [...page.matchAll(linksRE)];
+
+  const currOrigin = window.location.origin;
+
+  for(let nl of newLinks) {
+    if(nl.length > 1 && nl[1].startsWith(currOrigin) && !nl[1].match(/\?/)) {
+      queue.enqueue(nl[1]);
+    }
+  }
+
+}
+
+
+async function webgrep(pattern) {
   globalStop = false;
   window.addEventListener('mousedown',(e) => {
+    console.log('Stopping webgrep!');
     globalStop = true;
   });
 
 
   let visited = new Set();
-  const urlQueue = Queue.fromArray([window.location]);
+  const urlQueue = Queue.fromArray([window.location.toString()]);
   while(!urlQueue.isEmpty() && !globalStop) {
     const currLink = urlQueue.front().slice();
     urlQueue.dequeue();
+    if(visited.has(currLink)) {
+      continue;
+    }
     
+    let currPage = undefined;
+    await axios.get(currLink)
+      .then(value => {
+        currPage = value.data;
+      })
+      .catch(err => {
+        console.log('Error GETing ', currLink);
+      });
+
+    // Current page has been retrieved. Search text and
+    // scrape links.
+
+    if(currPage) {
+      processPage(currLink, currPage, 
+        urlQueue, visited, pattern);
+    }
   }
 
 }
 
+module.exports = webgrep;
